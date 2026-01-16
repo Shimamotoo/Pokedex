28908d9b585309f16002c59d50fa3310e42d3619
Bruno <bruno.mitsuhiroo@gmail.com>
2026-01-16 12:59:49 -0300

    Rota home privada

--- FrontEnd/src/routes/AppRoutes.tsx
+++ FrontEnd/src/routes/AppRoutes.tsx
@@ -2,13 +2,17 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import Home from "../pages/Home";
 import Pokedex from "../pages/Pokedex";
+import { PrivateRoute } from "./PrivateRoutes";
 
 export function AppRoutes() {
   return (
     <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home"element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>}/>
         <Route path="/pokedex" element={<Pokedex/>}/>
     </Routes>
   );
